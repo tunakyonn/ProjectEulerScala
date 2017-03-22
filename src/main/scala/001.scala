11e75fc71d213316dca77d5ca8a11fed5a7ed5ec
suzuki <tunakyonn@gmail.com>
2017-03-22 23:53:50 +0900

    Fix use function Problem 001

--- src/main/scala/001.scala
+++ src/main/scala/001.scala
@@ -3,13 +3,8 @@ package Problem001
 object Main {
   def main(args: Array[String]): Unit = {
     var sum = 0
-    for (i <- 1 until 1000)
-    {
-      if (i % 3 == 0 || i % 5 == 0) 
-      {
-        sum+=i
-      }
-    }
+    val isMultiple = (x: Int) => x % 3 == 0 || x % 5 == 0
+    for (i <- 1 until 1000; if isMultiple(i)) sum+=i
     println(sum)
   }
 }
