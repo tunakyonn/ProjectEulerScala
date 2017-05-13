e193aeec04932196068bcf611bc9c0d98f1ee05a
suzuki <tunakyonn@gmail.com>
2017-05-13 23:28:50 +0900

    Update 009.scala

--- src/main/scala/009.scala
+++ src/main/scala/009.scala
@@ -2,9 +2,12 @@ package Problem009
 
 object Main {
   def main(args: Array[String]): Unit = {
-    print(multi(1000))
+    multi(1000)
   }
   def multi(num: Int) = {
-
+    val a = (1 to 499).toList.find(a => (num * (500 - a)) % (num - a) == 0).get
+    val b = (num * (500 - a)) / (num - a)
+    val c = num - a - b
+    println(a * b * c)
   }
 }
