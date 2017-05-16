f2053ebbe507cc4a07f0ac7003ef604d3fad085e
suzuki <tunakyonn@gmail.com>
2017-05-16 22:25:51 +0900

    Update 010.scala

--- src/main/scala/010.scala
+++ src/main/scala/010.scala
@@ -2,16 +2,16 @@ package Problem010
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(primeNum(2000000))
+    primeNum(2000000)
   }
   def from(n: Int): Stream[Int] = Stream.cons(n, from(n+1))
   def sieve(n: Stream[Int]): Stream[Int] = Stream.cons(n.head, sieve(n.tail.filter(_ % n.head != 0)))
-  def primeNum(n :Int): Int = {
-    var answer = 0
+  def primeNum(n :Int) = {
+    var answer:Long = 0
     val primeNumber = sieve(from(2))
     for (i <- 1 to n) {
       answer += primeNumber.take(i).last
-      if (primeNumber.take(i).last > n) return answer
+      if (primeNumber.take(i).last > n)  println(answer)
     }
   }
 }
